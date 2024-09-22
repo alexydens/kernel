2b12b38f718f153f22fb5eded1689969ef780ad3
alexydens <alexydens@outlook.com>
2024-09-22 12:27:25 +0100

    ...

--- src/kernel.c
+++ src/kernel.c
@@ -25,7 +25,8 @@ void kernel_main(void) {
   /* Initialize GDT */
   gdt_init();
   /* Initialize vga text terminal */
-  vga_text_set_color(VGA_TEXT_COLOR_WHITE, VGA_TEXT_COLOR_DARK_GREY);
+  /*vga_text_set_color(VGA_TEXT_COLOR_GREEN, VGA_TEXT_COLOR_BLACK);*/
+  vga_text_set_color(VGA_TEXT_COLOR_LIGHT_GREY, VGA_TEXT_COLOR_BLUE);
   vga_text_set_cursor(0, 0);
   vga_text_clear(0);
   vga_text_puts("===> Initialized Global Descriptor Table.\r\n");
