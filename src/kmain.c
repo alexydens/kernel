5ccd35da660ad08b189a35f5b7b61e389a164f33
alexydens <alexydens@outlook.com>
2024-10-17 18:05:29 +0100

    Yes, I did just use a goto. What of it?

--- src/kmain.c
+++ src/kmain.c
@@ -12,20 +12,17 @@ void kernel_main(u32 mb_info_ptr) {
   /* Multiboot info */
   multiboot_info_t *mb_info = (multiboot_info_t *)(mb_info_ptr+0xc0000000);
 
-  /* Init */
-  if (!serial_init(12)) __asm__ __volatile__ ("int $0x3");
-  if (!gdt_init()) __asm__ __volatile__ ("int $0x3");
-  if (!idt_init()) __asm__ __volatile__ ("int $0x3");
+  /* Initialize */
+  if (!serial_init(12)) goto init_err;
+  serial_printf("===> Initialized serial port I/O\r\n");
+  if (!gdt_init()) goto init_err;
+  serial_printf("===> Initialized Global Descriptor Table\r\n");
+  if (!idt_init()) goto init_err;
+  serial_printf("===> Initialized Interrupt Descriptor Table\r\n");
 
   /* Test exceptions */
   //__asm__ __volatile__ ("int $0x3");
   //*(u32 *)0xdeadbeef = 0xdeadbeef;
-
-  /* Print some info about the framebuffer */
-  serial_printf("framebuffer_addr: 0x%08x\r\n", mb_info->framebuffer_addr);
-  serial_printf("framebuffer_width: 0x%08x\r\n", mb_info->framebuffer_width);
-  serial_printf("framebuffer_height: 0x%08x\r\n", mb_info->framebuffer_height);
-  serial_printf("framebuffer_bpp: 0x%08x\r\n", mb_info->framebuffer_bpp);
 
   /* Halt */
   while (1);
@@ -34,4 +31,9 @@ void kernel_main(u32 mb_info_ptr) {
       "1:\n\t"
       "hlt\n\t"
       "jmp 1b");
+
+  /* Error */
+  init_err:
+  serial_printf("Initialization error!\r\n");
+  __asm__ __volatile__ ("cli;hlt");
 }
