bea317a5f4057bbe36fee0edd9b1b21c6dfd7271
alexydens <alexydens@outlook.com>
2024-10-18 11:50:18 +0100

    Re-wrote the page frame allocator. There are no 'reserved' pages anymore, eveything is either 'free' or 'used

--- src/mem_pfa.c
+++ src/mem_pfa.c
@@ -4,24 +4,17 @@
 /* Consts */
 /* Size of a page in bytes */
 #define PAGE_SIZE             0x1000
-/* Status of a page frame */
-#define PAGE_STATUS_FREE      0
-#define PAGE_STATUS_USED      1
-#define PAGE_STATUS_RESERVED  2
 
 /* Globals */
-/*
- * The bitmap for the page frame allocator: 2 bits per page frame, so 4 in
- * each u8.
- */
-static u8 bitmap[0x100000000/(PAGE_SIZE*4)];
+/* PFA bitmap: 0 = free page, 1 = used page */
+static u8 bitmap[0x100000000/(PAGE_SIZE*8)];
 
 /* Initialize the page frame allocator */
 bool pfa_init(multiboot_info_t *mb_info) {
-  /* Zero out the bitmap */
-  memset(bitmap, 0, sizeof(bitmap));
+  /* Set the bitmap to all used */
+  memset(bitmap, 0xff, sizeof(bitmap));
 
-  /* Set all reserved pages as reserved */
+  /* Set all available pages to free */
   serial_printf("MEMORY MAP:\r\n");
   for (
       u32 i = 0;
@@ -39,25 +32,18 @@ bool pfa_init(multiboot_info_t *mb_info) {
         entry->type == MULTIBOOT_MEMORY_AVAILABLE ? "available" : "reserved"
     );
     if (
-        entry->type != MULTIBOOT_MEMORY_AVAILABLE
-        || (u64)entry->addr_low + entry->len_low > 0x100000000
-        || entry->addr_high
-        || entry->addr_low < 0x10000
+        entry->type == MULTIBOOT_MEMORY_AVAILABLE
+        && ((u64)entry->addr_low + entry->len_low < 0x100000000)
+        && (entry->addr_high == 0)
+        && (entry->addr_low > 0x10000)
     ) {
-      /* Mark this entry as reserved */
+      /* Mark this entry as available */
       for (
-          u32 addr = (u32)entry->addr_low;
-          addr < (u32)entry->addr_low + entry->len_low;
+          u32 addr = entry->addr_low;
+          addr < entry->addr_low + entry->len_low;
           addr += PAGE_SIZE
       ) {
-        bitmap[addr/(PAGE_SIZE*4)] |=
-          PAGE_STATUS_RESERVED << (((addr/PAGE_SIZE)%4)*2);
-        //serial_printf(
-            //"index: %u offset: %u result: 0x%02x\r\n",
-            //addr/(PAGE_SIZE*4),
-            //(addr/PAGE_SIZE)%4,
-            //bitmap[addr/(PAGE_SIZE*4)]
-        //);
+        bitmap[addr/(PAGE_SIZE*8)] &= ~(1 << ((addr/PAGE_SIZE)%8));
       }
     }
   }
@@ -68,11 +54,16 @@ bool pfa_init(multiboot_info_t *mb_info) {
 /* Get a free page frame */
 u32 pfa_get_frame(void) {
   /* Find a free page */
-  for (u32 i = 0; i < 0x100000000/(PAGE_SIZE*4); i++) {
-    for (u32 j = 0; j < 4; j++) {
-      if ((bitmap[i] & (3 << (j*2))) == 0) {
-        bitmap[i] |= PAGE_STATUS_USED << (j*2);
-        return (i*4+j)*PAGE_SIZE;
+  for (
+      u32 i = 0;
+      i < 0x100000000/(PAGE_SIZE*8);
+      i++
+  ) {
+    if (bitmap[i] == 0xff) continue;
+    for (u32 j = 0; j < 8; j++) {
+      if (!(bitmap[i] & (1 << j))) {
+        bitmap[i] |= 1 << j;
+        return (i*8+j)*PAGE_SIZE;
       }
     }
   }
@@ -80,6 +71,5 @@ u32 pfa_get_frame(void) {
 }
 /* Mark a page frame as free */
 void pfa_free_frame(u32 frame) {
-  bitmap[frame/(PAGE_SIZE*4)] &=
-    ~(PAGE_STATUS_USED << ((frame%(PAGE_SIZE*4))*2));
+  bitmap[frame/(PAGE_SIZE*8)] |= 1 << ((frame/PAGE_SIZE)%8);
 }
